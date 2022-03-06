025b02e0a28993116efa422bcfa6129353a19153
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2022-03-05 21:57:48 -0500

    Refator: All components are class based

--- src/components/scroll.component.jsx
+++ src/components/scroll.component.jsx
@@ -1,15 +1,19 @@
-const Scroll = (props) => {
-  return (
-    <div
-      style={{
-        overflowY: "scroll",
-        border: "1px solid black",
-        height: "90vh",
-      }}
-    >
-      {props.children}
-    </div>
-  );
-};
+import { Component } from "react";
+
+class Scroll extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          overflowY: "scroll",
+          border: "1px solid black",
+          height: "90vh",
+        }}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 export default Scroll;
